0d72f5d94c8fe83ef8732755ebda1414173ee2d9
George Zhang <georgeluzhang@berkeley.edu>
2018-12-22 20:25:39 -0800

    redid again lol

---
+++ js/lines.js
@@ -0,0 +1,104 @@
+var width = $(document).width();
+var height = $(document).height();
+
+function getStartingLocation(direction) {
+	if (direction == "left") {
+		start = "right";
+	} else if (direction == "right") {
+		start = "left";
+	} else if (direction == "up") {
+		start = "bottom";
+	} else if (direction == "down") {
+		start = "top";
+	} else {
+		console.log("direction error");
+	}
+	return start;
+}
+
+function moveDirection(id, direction, endPercent, offsetSize, duration) {
+	if (direction == "left") {
+		moveLeft(id, endPercent, offsetSize, duration);
+	} else if (direction == "right") {
+		moveRight(id, endPercent, offsetSize, duration);
+	} else if (direction == "up") {
+		moveUp(id, endPercent, offsetSize, duration);
+	} else if (direction == "down") {
+		moveDown(id, endPercent, offsetSize, duration);
+	} else {
+		console.log("direction error");
+	}
+}
+
+function move(id, direction, startPercent, endPercent, duration, offsetSize) {
+	start = getStartingLocation(direction);
+	$(id).css(start, startPercent * offsetSize * 10);
+	$(id).css("opacity", 1);
+	console.log("id: ", id);
+	console.log("direction: ", direction);
+	console.log("start: ", start);
+	console.log("startPercent: ", startPercent);
+	console.log("offsetSize", offsetSize);
+	moveDirection(id, direction, endPercent, width, duration);
+	console.log("done moving");
+}
+
+function moveRight(id, endPercent, offsetSize, duration) {
+	$(id).animate(
+	  {
+	    left: endPercent * offsetSize
+	  },
+	  duration
+	);
+}
+
+function moveLeft(id, endPercent, offsetSize, duration) {
+	$(id).animate(
+	  {
+	    right: endPercent * offsetSize
+	  },
+	  duration
+	);
+}
+
+function moveUp(id, endPercent, offsetSize, duration) {
+	$(id).animate(
+	  {
+	    bottom: endPercent * offsetSize
+	  },
+	  duration
+	);
+}
+
+function moveDown(id, endPercent, offsetSize, duration) {
+	$(id).animate(
+	  {
+	    top: endPercent * offsetSize
+	  },
+	  duration
+	);
+
+	console.log("id: ", id);
+	console.log("endPercent: ", endPercent);
+	console.log("offsetSize", offsetSize);
+	console.log("offsetSize", duration);
+}
+
+
+// Make text disappear while loading lines
+window.setTimeout(function () {
+	$(".text").animate({opacity: 1}, 1500);
+}, 3000);
+
+
+// Vertical Lines
+window.setTimeout(function () {move("#box1-line", "down", -.1, 0, 1500, height);} ,1000);
+window.setTimeout(function () {move("#box2-line", "up", -.1, 0, 1500, height);} , 0);
+window.setTimeout(function () {move("#box3-line", "down", -.1, 0, 1500, height);} ,500);
+
+// Horizonatal Lines
+window.setTimeout(function () {move("#about-line", "right", -.1, 0, 1500, width * .25);} ,1250);
+window.setTimeout(function () {move("#links-line", "right", -.1, 0, 1500, width * .215);} ,1000);
+window.setTimeout(function () {move("#more-links-line-left", "left", -.1, 0, 1500, width * .25);} ,1500);
+window.setTimeout(function () {move("#more-links-line-right", "right", -.1, 0, 1500, width * .25);} ,1500);
+window.setTimeout(function () {move("#contact-line", "left", -.1, 0, 1500, width * .25);} , 2000);
--- js/plane.js
+++ js/plane.js
@@ -1,54 +1,54 @@
 var width = $(document).width();
 
-// return to original position
-function goRight() {
-  $("#plane").css("left", width * 0.7);
-  // $("#plane").animate(
-  //   {
-  //     left: width
-  //   },
-  //   2000
-  // );
-}
+// // return to original position
+// function goRight() {
+//   $("#plane").css("left", width * 0.7);
+//   // $("#plane").animate(
+//   //   {
+//   //     left: width
+//   //   },
+//   //   2000
+//   // );
+// }
 
-// go left and then run go right
-function goLeft() {
-  $("#plane").animate(
-    {
-      left: -(width * 0.8)
-    },
-    7000,
-    function() {
-      setTimeout(goRight, 50);
-    }
-  );
-  //setTimeout(goRight, 50);
-}
+// // go left and then run go right
+// function goLeft() {
+//   $("#plane").animate(
+//     {
+//       left: -(width * 0.8)
+//     },
+//     7000,
+//     function() {
+//       setTimeout(goRight, 50);
+//     }
+//   );
+//   //setTimeout(goRight, 50);
+// }
 
-function move(e) {
-  dis();
-  setTimeout(goLeft, 50);
-}
+// function move(e) {
+//   dis();
+//   setTimeout(goLeft, 50);
+// }
 
-// disable button
-function dis() {
-  document.getElementById("vroom").disabled = true;
-  setTimeout(fadeBack, 7200);
-}
+// // disable button
+// function dis() {
+//   document.getElementById("vroom").disabled = true;
+//   setTimeout(fadeBack, 7200);
+// }
 
-function fadeBack() {
-  unfade(document.getElementById("vroom"));
-  setTimeout(function() {
-    document.getElementById("vroom").disabled = false;
-  }, 100);
-}
+// function fadeBack() {
+//   unfade(document.getElementById("vroom"));
+//   setTimeout(function() {
+//     document.getElementById("vroom").disabled = false;
+//   }, 100);
+// }
 
-document.getElementById("vroom").onclick = move;
+// document.getElementById("vroom").onclick = move;
 
-$("#plane").css("left", width * 0.7);
-// $("#plane").animate(
-//   {
-//     left: width * 0.01
-//   },
-//   2600
-// );
+$("#plane").css("left", width * -.7);
+$("#plane").animate(
+  {
+    left: width * 1
+  },
+  3500
+);
