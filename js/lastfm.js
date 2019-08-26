3ba2e74e1e3d553710d351627010c12defc8081c
George Zhang <georgeluzhang@berkeley.edu>
2019-08-25 20:55:07 -0700

    wip...

--- js/lastfm.js
+++ js/lastfm.js
@@ -13,28 +13,4 @@ function getRecentTracks(limit) {
     })
 };
 
-$("#more-button").click(function() {
-    $(".more").html(
-    'Some of my favorite YouTube videos are:' +  
-    '<ul>' +
-        '<li><a href="https://www.youtube.com/watch?v=amv8fiCQnXI" target="_blank">Joel Embiid on Hot Ones</a></li>' + 
-        '<li><a href="https://www.youtube.com/watch?v=Vs6In7UtyXY" target="_blank">Yes Theory Fools the Internet</a></li>' +
-        '<li><a href="https://www.youtube.com/watch?v=gQlKelwPX4A" target="_blank">How Calvin Harris made Slide</a></li>' +
-    '</ul>' + 
-    '<p></p>' + 
-
-    'The last three songs I listened to were:' + 
-    currSongs + 
-    '<p></p>' + 
-
-    'My current top movies are: ' + 
-    '<ul>' + 
-        '<li><a href="https://www.youtube.com/watch?v=c3sBBRxDAqk" target="_blank">Ratatouille</a> (2007)</li>' + 
-        '<li><a href="https://www.youtube.com/watch?v=ijXruSzfGEc" target="_blank">The Prestige</a> (2006)</li>' + 
-        '<li>with Crazy Rich Asians (2018) and Into the Spider-Verse (2018) on the come-up</li>' + 
-    '</ul>' + 
-    '<p></p>'
-    );
-})
-
 $(document).ready(getRecentTracks(3));
